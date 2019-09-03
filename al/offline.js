4ef340e4c265aeb7082732a7ac7c0f575d9a7ba2
NlinduOS <54837243+NlinduOS@users.noreply.github.com>
2019-09-03 22:40:13 +0300

    Add files via upload

--- al/offline.js
+++ al/offline.js
@@ -1,12 +1,11 @@
 ﻿{
-	"version": 1567517966,
+	"version": 1567539533,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
 		"images/bird-sheet0.png",
-		"images/bird-sheet1.png",
 		"images/tiledbackground.png",
 		"images/tiledbackground2.png",
 		"images/background-sheet0.png",
@@ -14,6 +13,8 @@
 		"images/toppipe-sheet0.png",
 		"images/sprite-sheet0.png",
 		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
